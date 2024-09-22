488d233497513f291ec9db0fd40a51c3716fe1c6
Avance_Sem3 <fabriciosubilete@hotmail.com>
2024-09-22 12:12:16 -0500

    Nube

--- src/main/java/pe/edu/upc/urtianguis_backend/dtos/RolDTO.java
+++ src/main/java/pe/edu/upc/urtianguis_backend/dtos/RolDTO.java
@@ -1,10 +1,11 @@
 package pe.edu.upc.urtianguis_backend.dtos;
 
-import jakarta.persistence.Column;
+import pe.edu.upc.urtianguis_backend.entities.Usuario;
 
 public class RolDTO {
     private int Id_rol;
     private String Tipo_rol;
+    private Usuario Us;
     public int getId_rol() {
         return Id_rol;
     }
@@ -16,5 +17,13 @@ public class RolDTO {
     }
     public void setTipo_rol(String tipo_rol) {
         Tipo_rol = tipo_rol;
+    }
+
+    public Usuario getUs() {
+        return Us;
+    }
+
+    public void setUs(Usuario us) {
+        Us = us;
     }
 }
