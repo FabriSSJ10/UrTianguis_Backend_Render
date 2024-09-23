b5ebc1482f872849f46388d916d4c61f96efca8f
Avance_Sem3 <fabriciosubilete@hotmail.com>
2024-09-23 15:30:24 -0500

    Nube

--- src/main/java/pe/edu/upc/urtianguis_backend/dtos/Tipo_OcasionDTO.java
+++ src/main/java/pe/edu/upc/urtianguis_backend/dtos/Tipo_OcasionDTO.java
@@ -7,4 +7,19 @@ public class Tipo_OcasionDTO {
     private int id_tipo_ocasion;
     private String Tipo_ocasion;
 
+    public int getId_tipo_ocasion() {
+        return id_tipo_ocasion;
+    }
+
+    public void setId_tipo_ocasion(int id_tipo_ocasion) {
+        this.id_tipo_ocasion = id_tipo_ocasion;
+    }
+
+    public String getTipo_ocasion() {
+        return Tipo_ocasion;
+    }
+
+    public void setTipo_ocasion(String tipo_ocasion) {
+        Tipo_ocasion = tipo_ocasion;
+    }
 }
